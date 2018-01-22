773316b8dacfbe499df787292724d0ae69550073
rdegosse <35624447+rdegosse@users.noreply.github.com>
2018-01-22 19:35:28 +0100

    test1

---
+++ Test-Farmware/script.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+
+from CeleryPy import log
+# from DB import DB
+
+
+
+
+if __name__ == "__main__":
+
+    log("Start script.py...", message_type='info', title='Test-Farmware')
+
+    
+
+#    plantdb= DB()
+
+ #   plantdb.load_plants_from_web_app()   #Get plant points from Webapp
+ #   plantdb.count_downloaded_plants()    #Print Plantcount in log
+ #   plantdb.load_sequences_from_app()    #Get sequences and determine the sequence id
+ #   plantdb.loop_plant_points()          #Move to plant points and water them with the Water on/off sequence
+
+ 
+    log("End script.py...", message_type='info', title='Test-Farmware')
