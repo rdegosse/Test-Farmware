3f65d1a7571fc5d204ddfc22da05924ba2f9a454
rdegosse <35624447+rdegosse@users.noreply.github.com>
2018-02-03 23:58:34 +0100

    test algo

--- Test-Farmware/Util.py
+++ Test-Farmware/Util.py
@@ -1,42 +1,140 @@
+import copy
 from math import sqrt
-    
-def get_optimal_way(points):
+from operator import itemgetter
+
+def Get_Optimal_Way(points):
     #return list of dict points sorted , tab of sorted id
     #p,t = get_optimal_way(points)
         
-    #create distance graph with preference (*1.1) for y move
+    #create distance graph with preference for y move (x*1.000001)
     graph = {}
     neighb = {}
     for psrc in points:
         neighb.clear()
         for pdst in points:
             if psrc["id"] != pdst["id"]:
-                neighb[pdst["id"]] = sqrt(((psrc["x"]-pdst["x"])**2)*1.1+((psrc["y"]-pdst["y"])**2))
+                neighb[pdst["id"]] = sqrt(((psrc["x"]-pdst["x"])**2)*1.000001+((psrc["y"]-pdst["y"])**2))
         graph[psrc["id"]] = neighb.copy()
+
+    #sort each each dict in dict and keep original copy
+    for e in graph:
+        graph[e] = dict(sorted(graph[e].items(),key=itemgetter(1)))
+    graphcopy = copy.deepcopy(graph)
 
     #sort for min and max points
     points = sorted(points , key=lambda elem: "%02d %s" % (elem['y'], elem['x']))
     id_start = points[0]["id"]
     id_end = points[len(points)-1]["id"]
 
-    #get optimal way tab id
+    #debug
+    #id_start = 426
+    #id_end = 465
+
+    ####DEBUG
+    #print(id_start)
+    #print(id_end)
+
+    #for p in points:
+    #    print(p)
+    print(len(points))
+    ####DEBUG
+
+
+    #get optimal way tab id (shortest way with max points)
+    #print("----------------------------")
     tab = []
     id_cur = id_start
+    
     while 1:
         tab.append(id_cur)
         if id_cur == id_end:
             break
+        id_next = min(graph[id_cur], key=graph[id_cur].get)
+        
+        #print(id_cur)
+        #print(id_next)
+        #print(graph[id_cur])
+        
         for i in graph:
             if id_cur in graph[i]:
-                del graph[i][id_cur]    
-        id_cur = min(graph[id_cur], key=graph[id_cur].get)
+                del graph[i][id_cur]
+        id_cur = id_next    
+    
+    #insert lost points by proximity
+    tablost = []
+    for k in graph[id_end].keys():
+        tablost.append(k)
+
+    #debug / security
+    no_infinite = 5
+    #debug
+
+    while len(tablost) != 0:
+        if no_infinite == 0:
+            break
+
+        #print(graphcopy)
+        #print(tablost)
+
+        for i in tablost:
+            for m in graphcopy[i]:
+                id_prox = m
+                #print(id_prox)
+                try:
+                    ind_id_prox = tab.index(id_prox)
+                    #print(ind_id_prox)
+                    if ind_id_prox >= 1:
+                        id_prox_bef = tab[ind_id_prox-1]
+                        dist_bef = graphcopy[i][id_prox_bef]
+                    else:
+                        dist_bef = float('inf')
+                    
+                    if ind_id_prox < len(tab)-1:
+                        id_prox_aft = tab[ind_id_prox+1]
+                        dist_aft = graphcopy[i][id_prox_aft]
+                    else:
+                        dist_aft = float('inf')
+                    
+                    if dist_bef < dist_aft:
+                        tab.insert(ind_id_prox,i)    
+                    else:
+                        tab.insert(ind_id_prox+1,i)
+                    
+                    tablost.remove(i)
+
+                    
+            
+                    for j in graph:
+                        if i in graph[j]:
+                            del graph[j][i]
+                    
+                    #print(graph[i])
+
+                    break
+                except:
+                    #print("error")
+                    pass
+    no_infinite -= 1
+
+    #print(tablost)      
+    #print("----------------------------")
+    #print(graph)
+    print(len(graph))
+    
 
     #create list of dictionary points in optimal way order
     opt_points = []
     for i in tab:
         p = [e for e in points if e["id"] == i]
-        opt_points.append(p.copy())
-
+        opt_points.append(p.copy()[0])
     return opt_points, tab
 
     
+def Filter_Points(points,name='',openfarm_slug='',pointer_type='Plant'):
+    filtered_points = []
+    for p in points:
+        if p['pointer_type'].lower() == pointer_type.lower() and (p['name'].lower() == name.lower() or name == '') and (p['openfarm_slug'].lower() == openfarm_slug.lower() or openfarm_slug == ''):
+            filtered_points.append(p.copy())
+    return filtered_points
+
+
--- Test-Farmware/script.py
+++ Test-Farmware/script.py
@@ -5,7 +5,7 @@ import base64
 import requests
 
 from CeleryPy import log
-from Util import get_optimal_way
+from Util import *
 
 # from DB import DB
 
@@ -18,6 +18,7 @@ if __name__ == "__main__":
         api_token = os.environ['API_TOKEN']
     except KeyError:
         api_token = 'no token'
+        api_token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJib3QiLCJzdWIiOjE0NiwiaWF0IjoxNTE3NTY4NjI5LCJqdGkiOiI4MTRjODkzZi1lMGQzLTRhMGMtOGZkNi03ODc0ZWE4NmE1YjkiLCJpc3MiOiIvLzEwLjEuMS45MDozMDAwIiwiZXhwIjoxNTIxMDI0NjI5LCJtcXR0IjoiMTAuMS4xLjkwIiwiYm90IjoiZGV2aWNlXzIxMSIsInZob3N0IjoiLyIsIm1xdHRfd3MiOiJ3czovLzEwLjEuMS45MDozMDAyL3dzIiwib3NfdXBkYXRlX3NlcnZlciI6Imh0dHBzOi8vYXBpLmdpdGh1Yi5jb20vcmVwb3MvRmFybUJvdC9mYXJtYm90X29zL3JlbGVhc2VzLzkyMDA5NDMiLCJpbnRlcmltX2VtYWlsIjoicmRlZ29zc2VAaW5pdGhpbmsuZnIiLCJmd191cGRhdGVfc2VydmVyIjoiREVQUkVDQVRFRCIsImJldGFfb3NfdXBkYXRlX3NlcnZlciI6Ik5PVF9TRVQifQ.K8Fv-Y2XuOEa_1ZoiwO3MZYk5X4FT8tvG6UKj2GTzwq2KpGU28DySaUa7z2AnfxI_L50Yuc2GviHVKkrhFAzum4mRKXpzIBkpHnLUSc_VvPdsYIXrU_cDn5nTDrZoO_okje0hepp6SBPtCyuimf6iijpergVCNUj8w-SNuft62ZnE_0f1JI8glTjCOSuPIVAoZmlYb2sd8_m9OSivqHbagunql2uAsgfY5-88Xsh7cn5fy04S_f04-AEi3iyEqchGjlfxWu7rPuPg7jSGbv0hYzBR36H2C2YOfbPrRQlleD5kom7JXYDlrdJPV3d48NlB3SJQGxtYYM9XmafAtf2YQ'
 			
     log(api_token, message_type='info', title='Test-Farmware')
 	
@@ -44,18 +45,26 @@ if __name__ == "__main__":
     response = requests.get(api_url + 'points', headers=headers)
     app_points = response.json()
     if response.status_code == 200:
-            opt_points, tab_id = get_optimal_way(app_points)
-    
-    log(tab_id, message_type='info', title='Test-Farmware')
-    
-    
-    
-#    plantdb= DB()
+        #app_points = Filter_Points(app_points,openfarm_slug='carrot')
+        app_points = Filter_Points(app_points)
 
- #   plantdb.load_plants_from_web_app()   #Get plant points from Webapp
- #   plantdb.count_downloaded_plants()    #Print Plantcount in log
- #   plantdb.load_sequences_from_app()    #Get sequences and determine the sequence id
- #   plantdb.loop_plant_points()          #Move to plant points and water them with the Water on/off sequence
+        opt_points,tab_id = Get_Optimal_Way(app_points)
 
- 
+        log(tab_id, message_type='info', title='Test-Farmware')
+        #log(opt_points, message_type='info', title='Test-Farmware')
+
+        for p in opt_points:
+            log(p, message_type='info', title='Test-Farmware')    
+
+        #log('', message_type='info', title='Test-Farmware')
+        #log(len(app_points), message_type='info', title='Test-Farmware')
+        #log(app_points, message_type='info', title='Test-Farmware')
+        #log('', message_type='info', title='Test-Farmware')
+        #log(len(tab_id), message_type='info', title='Test-Farmware')
+        #log(tab_id, message_type='info', title='Test-Farmware')
+        #log('', message_type='info', title='Test-Farmware')
+        #log(len(opt_points), message_type='info', title='Test-Farmware')
+        #log(opt_points, message_type='info', title='Test-Farmware')
+    
+    
     log('End script.py...', message_type='info', title='Test-Farmware')
