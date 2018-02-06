842125675bcfb29f47323e06bcecd497303f5818
rdegosse <35624447+rdegosse@users.noreply.github.com>
2018-02-06 01:29:55 +0100

    test

--- Test-Farmware/script.py
+++ Test-Farmware/script.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     FARMWARE_NAME = "Test-Farmware"
 
     log('Start script.py...', message_type='info', title=FARMWARE_NAME)
-"""	
+	
     try:
         farmware = FARMWARE(FARMWARE_NAME)
         #pass
@@ -21,6 +21,7 @@ if __name__ == "__main__":
             farmware.run()
             #pass
         except Exception as e:
-            log(e ,message_type='error', title=FARMWARE_NAME + " : run" )    
-"""    
+            log(e ,message_type='error', title=FARMWARE_NAME + " : run" )
+
     log('End script.py...', message_type='info', title=FARMWARE_NAME)
+    
